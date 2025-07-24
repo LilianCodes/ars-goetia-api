39b0695931b297acd646e7aebf499d8d43f35401
xnarisa <122071691+LilianCodes@users.noreply.github.com>
2025-07-23 19:19:47 -0700

    typo

--- src/main.py
+++ src/main.py
@@ -7,7 +7,7 @@ import random
 app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
-    allow_origin=['*'],
+    allow_origins=['*'],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"]
